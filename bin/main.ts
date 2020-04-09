ee5e65fc50d146677ab792ff480ec0b7e7039722
Timothy Purchas <timothy.purchas@iag.com.au>
2020-04-09 16:01:48 +1000

    Update Template to Cosmos 0.2.6

--- bin/main.ts
+++ bin/main.ts
@@ -5,7 +5,7 @@ import {
   CosmosStack,
   GalaxyStack,
   CiCdSolarSystemStack,
-  EcsSolarSystemStack
+  EcsSolarSystemStack,
 } from "@cdk-cosmos/core";
 
 // Cdk App
@@ -18,27 +18,32 @@ const devEnvConfig = { account: "2222", region: "ap-southeast-2" };
 // Create the Cosmos (Core)
 const cosmos = new CosmosStack(app, "Demo", {
   tld: "cosmos.com",
-  env: mgtEnvConfig
+  env: mgtEnvConfig,
 });
 
 // Create an Mgt Galaxy with cidr
-const mgtGalaxy = new GalaxyStack(cosmos, "Mgt", {
-  cidr: "10.0.0.0/22"
-});
+const mgtGalaxy = new GalaxyStack(cosmos, "Mgt");
 
 // Create the CiCd Solar System
-const ciCd = new CiCdSolarSystemStack(mgtGalaxy);
+const ciCd = new CiCdSolarSystemStack(mgtGalaxy, {
+  cidr: "10.0.0.0/24",
+});
 
 // Create an Dev Galaxy with cidr
 const devGalaxy = new GalaxyStack(cosmos, "Dev", {
-  cidr: "10.0.1.0/22",
-  env: devEnvConfig
+  cidr: "10.0.1.0/24",
+  env: devEnvConfig,
 });
+const devGalaxySharedVpc = devGalaxy.AddSharedVpc();
 
 // TODO: Enable Solar Systems after bootstrap
 
-// // Create an Dev SolarSystem which is Ecr capable
-// const dev = new EcsSolarSystemStack(devGalaxy, "Dev");
+// // Create an Dev Solar System which is Ecr capable
+// const dev = new EcsSolarSystemStack(devGalaxy, "Dev", {
+//   vpc: devGalaxySharedVpc,
+// });
 
-// // Create an Tst SolarSystem which is Ecr capable
-// const tst = new EcsSolarSystemStack(devGalaxy, "Tst");
+// // Create an Tst Solar System which is Ecr capable
+// const tst = new EcsSolarSystemStack(devGalaxy, "Tst", {
+//   vpc: devGalaxySharedVpc,
+// });
