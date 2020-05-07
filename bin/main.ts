8db5cf29be9dd5125b71243e9c689ebff601aa74
Tim P <timothy@purchas.com>
2020-05-07 14:43:58 +1000

    Update Cdk 1.37.0 + Cosmos 0.4.1 (#11)

--- bin/main.ts
+++ bin/main.ts
@@ -2,10 +2,10 @@
 import "source-map-support/register";
 import { App } from "@aws-cdk/core";
 import {
-  CosmosStack,
-  GalaxyStack,
-  CiCdSolarSystemStack,
-  EcsSolarSystemStack,
+  CosmosCoreStack,
+  GalaxyCoreStack,
+  CiCdSolarSystemCoreStack,
+  EcsSolarSystemCoreStack,
 } from "@cdk-cosmos/core";
 
 // Cdk App
@@ -16,34 +16,34 @@ const mgtEnvConfig = { account: "1111", region: "ap-southeast-2" };
 const devEnvConfig = { account: "2222", region: "ap-southeast-2" };
 
 // Create the Cosmos (Core)
-const cosmos = new CosmosStack(app, "Demo", {
+const cosmos = new CosmosCoreStack(app, "Demo", {
   tld: "cosmos.com",
   env: mgtEnvConfig,
 });
 
 // Create an Mgt Galaxy with cidr
-const mgtGalaxy = new GalaxyStack(cosmos, "Mgt");
+const mgtGalaxy = new GalaxyCoreStack(cosmos, "Mgt");
 
 // Create the CiCd Solar System
-const ciCd = new CiCdSolarSystemStack(mgtGalaxy, {
+const ciCd = new CiCdSolarSystemCoreStack(mgtGalaxy, {
   cidr: "10.0.0.0/24",
 });
 
 // Create an Dev Galaxy with cidr
-const devGalaxy = new GalaxyStack(cosmos, "Dev", {
+const devGalaxy = new GalaxyCoreStack(cosmos, "Dev", {
   cidr: "10.0.1.0/24",
   env: devEnvConfig,
 });
-const devGalaxySharedVpc = devGalaxy.AddSharedVpc();
+const devGalaxySharedVpc = devGalaxy.addSharedVpc();
 
 // TODO: Enable Solar Systems after bootstrap
 
 // // Create an Dev Solar System which is Ecr capable
-// const dev = new EcsSolarSystemStack(devGalaxy, "Dev", {
+// const dev = new EcsSolarSystemCoreStack(devGalaxy, "Dev", {
 //   vpc: devGalaxySharedVpc,
 // });
 
 // // Create an Tst Solar System which is Ecr capable
-// const tst = new EcsSolarSystemStack(devGalaxy, "Tst", {
+// const tst = new EcsSolarSystemCoreStack(devGalaxy, "Tst", {
 //   vpc: devGalaxySharedVpc,
 // });
