74764d79fc37c77f215977d2e8107c04c1998028
Tim P <timothy@purchas.com>
2020-10-01 09:08:03 +1000

    Update Cosmos 0.6.4 (#19)

--- bin/main.ts
+++ bin/main.ts
@@ -1,37 +1,33 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import { App } from "@aws-cdk/core";
-import {
-  CosmosCoreStack,
-  GalaxyCoreStack,
-  SolarSystemCoreStack,
-} from "@cdk-cosmos/core";
+import 'source-map-support/register';
+import { App } from '@aws-cdk/core';
+import { CosmosCoreStack, GalaxyCoreStack, SolarSystemCoreStack } from '@cdk-cosmos/core';
 
 // Cdk App
 const app = new App();
 
 // Aws Env Config
-const mgtEnvConfig = { account: "1111", region: "ap-southeast-2" };
-const devEnvConfig = { account: "2222", region: "ap-southeast-2" };
+const mgtEnvConfig = { account: '1111', region: 'ap-southeast-2' };
+const devEnvConfig = { account: '2222', region: 'ap-southeast-2' };
 
 // Create the Cosmos (Core)
-const cosmos = new CosmosCoreStack(app, "Demo", {
-  tld: "cosmos.com",
+const cosmos = new CosmosCoreStack(app, 'Demo', {
+  tld: 'cosmos.com',
   env: mgtEnvConfig,
 });
 
 // Create an Mgt Galaxy with cidr
-const mgtGalaxy = new GalaxyCoreStack(cosmos, "Mgt");
+const mgtGalaxy = new GalaxyCoreStack(cosmos, 'Mgt');
 
 // Create the CiCd Solar System
-const ciCd = new SolarSystemCoreStack(mgtGalaxy, "CiCd", {
-  cidr: "10.0.0.0/24",
+const ciCd = new SolarSystemCoreStack(mgtGalaxy, 'CiCd', {
+  cidr: '10.0.0.0/24',
 });
 ciCd.addCiCd();
 
 // Create an Dev Galaxy with cidr
-const devGalaxy = new GalaxyCoreStack(cosmos, "Dev", {
-  cidr: "10.0.1.0/24",
+const devGalaxy = new GalaxyCoreStack(cosmos, 'Dev', {
+  cidr: '10.0.1.0/24',
   env: devEnvConfig,
 });
 devGalaxy.addSharedVpc();
